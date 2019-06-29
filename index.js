923c6b9b489499d8e66a58d3d65e43daee1869ec
Catherine <cathy.korres@gmail.com>
2019-06-29 22:22:55 +0000

    User listing routes

--- index.js
+++ index.js
@@ -2,8 +2,22 @@ const express = require('express');
 
 const server = express();
 
-server.get('/test', (req, res) =>{
-  return res.json({ message:'Hello World' });
+// Query params = ?test=1
+// Route params = /users/1
+// Request body = { "name":"Catherine", "email":"email@domain.com" } 
+
+// CRUD = Create, Read, Update, Delete
+
+const users = ['Catherine', 'Filipe', 'Frank']; 
+
+server.get('/users', (req, res) => {
+  return res.json(users);
+});
+
+server.get('/users/:index', (req, res) =>{
+  const { index } = req.params;
+
+  return res.json(users[index]);
 });
 
 server.listen(3000);
